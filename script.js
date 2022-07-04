d51b9a006b45e618351bdc0a983c271ac778df8e
auz <lankyauz@gmail.com>
2022-07-04 15:08:04 -0600

    Added placeholder styles. Setup some quick html elements.

---
+++ script.js
@@ -0,0 +1,17 @@
+const library = [];
+let tempBook;
+
+function Book(title,author,pageCount,hasRead){
+    this.title = title
+    this.author = author
+    this.pageCount = pageCount
+    this.hasRead = hasRead
+};
+
+function addBookToLibrary(x){
+    library.push(x);
+};
+// loops through library array and displays each book on the page.
+function displayBooks(){
+
+};
